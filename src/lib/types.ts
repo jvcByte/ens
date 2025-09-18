c1805da197d2ac310ca237859cac222d41cf32f9
jvcbyte <jvc8463@gmail.com>
2025-09-18 23:34:38 +0100

    update readme, remove search provider used in my previous project

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,42 +1,5 @@
-import type { LinkProps } from "@tanstack/react-router";
 import { QueryClient } from "@tanstack/react-query";
 
 export interface RouterContext {
   queryClient?: QueryClient;
 }
-
-type User = {
-  name: string;
-  email: string;
-  avatar: string;
-};
-
-type BaseNavItem = {
-  title: string;
-  badge?: string;
-  icon?: React.ElementType;
-};
-
-type NavLink = BaseNavItem & {
-  url: LinkProps["to"];
-  items?: never;
-};
-
-type NavCollapsible = BaseNavItem & {
-  items: (BaseNavItem & { url: LinkProps["to"] })[];
-  url?: undefined;
-};
-
-type NavItem = NavCollapsible | NavLink;
-
-type NavGroup = {
-  title: string;
-  items: NavItem[];
-};
-
-type SidebarData = {
-  user: User;
-  navGroups: NavGroup[];
-};
-
-export type { SidebarData, NavGroup, NavItem, NavCollapsible, NavLink };
