6b2959480d1265fe29e0d7cca83f6cd66df78a9f
jvcbyte <jvc8463@gmail.com>
2025-09-22 01:01:25 +0100

    header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -37,11 +37,16 @@ export const Header = () => {
 
                 <button
                   onClick={() => setMenuState(!menuState)}
-                  aria-label={menuState == true ? "Close Menu" : "Open Menu"}
-                  className="relative z-20 -m-2.5 -mr-4 block cursor-pointer p-2.5 lg:hidden"
+                  aria-label={menuState ? "Close Menu" : "Open Menu"}
+                  aria-expanded={menuState}
+                  data-state={menuState ? "active" : "closed"}
+                  className="relative z-20 block cursor-pointer p-2.5 lg:hidden"
                 >
-                  <Menu className="in-data-[state=active]:rotate-180 in-data-[state=active]:scale-0 in-data-[state=active]:opacity-0 m-auto size-6 duration-200" />
-                  <X className="in-data-[state=active]:rotate-0 in-data-[state=active]:scale-100 in-data-[state=active]:opacity-100 absolute inset-0 m-auto size-6 -rotate-180 scale-0 opacity-0 duration-200" />
+                  {menuState ? (
+                    <X className="m-auto size-6 duration-200" />
+                  ) : (
+                    <Menu className="m-auto size-6 duration-200" />
+                  )}
                 </button>
               </div>
 
@@ -60,7 +65,10 @@ export const Header = () => {
               </div>
             </div>
 
-            <div className="bg-background in-data-[state=active]:block lg:in-data-[state=active]:flex mb-6 hidden w-full flex-wrap items-center justify-end space-y-8 rounded-3xl border p-6 shadow-2xl shadow-zinc-300/20 md:flex-nowrap lg:m-0 lg:flex lg:w-fit lg:gap-6 lg:space-y-0 lg:border-transparent lg:bg-transparent lg:p-0 lg:shadow-none dark:shadow-none dark:lg:bg-transparent">
+            <div
+              data-state={menuState ? "active" : "closed"}
+              className={`${menuState ? "block" : "hidden"} bg-background mb-6 w-full flex-wrap items-center justify-end space-y-8 rounded-3xl border p-6 shadow-2xl shadow-zinc-300/20 md:flex-nowrap lg:m-0 lg:flex lg:w-fit lg:gap-6 lg:space-y-0 lg:border-transparent lg:bg-transparent lg:p-0 lg:shadow-none dark:shadow-none dark:lg:bg-transparent z-30`}
+            >
               <div className="lg:hidden">
                 <ul className="space-y-6 text-base">
                   {menuItems.map((item, index) => (
@@ -76,7 +84,9 @@ export const Header = () => {
                 </ul>
               </div>
               <div className="flex w-full flex-col space-y-3 sm:flex-row sm:gap-3 lg:gap-6 sm:space-y-0 md:w-fit">
-                <WalletConnectionButton />
+                <div className="hidden lg:block">
+                  <WalletConnectionButton />
+                </div>
                 <div className="hidden lg:block">
                   <ThemeSwitcher />
                 </div>
